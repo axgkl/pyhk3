e432ccbbaed2a595c049dfa7ca0ca982490dc347
Gunther Klessinger <gunther.klessinger@axiros.com>
2025-02-20 14:18:37 +0100

    fix: import

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,11 +1,11 @@
-from pyhk3 import create, hk3s
-
-
-def test_render():
-    c = hk3s.render_config()
-    print(c)
-    assert 'master' in c
+from pyhk3 import create
 
 
 def test_import():
     assert create is not None
+
+
+def test_render():
+    c = create.hk3s.render_config()
+    print(c)
+    assert 'master' in c
