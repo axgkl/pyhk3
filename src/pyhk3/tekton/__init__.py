205f60eb16fa7ffc04cfb26291018e3c397ba288
Gunther Klessinger <gk@mba.local>
2025-07-02 02:49:03 +0200

    tekton fix and removal from repo

--- src/pyhk3/tekton/__init__.py
+++ src/pyhk3/tekton/__init__.py
@@ -1,5 +1,5 @@
-import os
-
+import os, yaml
+import shutil
 from ..ssh import ensure_forward
 from ..tools import require_cmd, confirm, die, log, read_file, run, const, shw, write_file
 from ..tools import need_env as E
@@ -41,6 +41,15 @@ class tekton:
         run('flux reconcile kustomization infra-tekton -n flux-system')
 
     def remove_from_cluster():
+        d_our_repo = tools.get_clean_repo()
+        fn_entry = d_our_repo + '/clusters/production/infrastructure.yaml'
+        docs = read_file(fn_entry, '').split('\n---\n')
+        docs = '\n---\n'.join([doc for doc in docs if 'infra-tekton' not in doc])
+        with open(fn_entry, 'w') as f:
+            f.write(docs + '\n')
+        d = d_our_repo + '/infrastructure/tekton'
+        if os.path.exists(d):
+            shutil.rmtree(d)
         for l in [
             """for crd in $(kubectl get crd | grep -i tekton | awk '{print $1}'); do kubectl patch crd $crd -p '{"metadata":{"finalizers":[]}}' --type=merge; done""",
             """kubectl get crd | grep -i tekton | awk '{print $1}' | xargs -I {} kubectl delete crd {}""",
@@ -50,7 +59,8 @@ class tekton:
             'kubectl get crd| grep -i tekton',
         ]:
             log.info(l) or os.system(l)
-            log.info('You can remove all tekton from your flux repo and reconcile now')
+        tools.git(d_our_repo, add='.', msg='Tekton removed', push=True)
+        log.info('Removed all tekton from your cluster and flux repo')
 
 
 T = """
@@ -58,7 +68,7 @@ T = """
 apiVersion: kustomize.toolkit.fluxcd.io/v1
 kind: Kustomization
 metadata:
-  name: infra-tekton
+  name: infra-tekton-operator
   namespace: flux-system
 spec:
   dependsOn:
@@ -76,16 +86,15 @@ spec:
     provider: sops
     secretRef:
       name: sops-age
-
 ---
 apiVersion: kustomize.toolkit.fluxcd.io/v1
 kind: Kustomization
 metadata:
-  name: infra-tekton
+  name: infra-tekton-config
   namespace: flux-system
 spec:
   dependsOn:
-    - name: infra-tekton
+    - name: infra-tekton-operator  # Wait for operator to be ready
   interval: 1h
   retryInterval: 1m
   timeout: 5m
